774a0b89e85c9de99679ab660bc99b734b4ccc0d
YamamotoU518 <hw37hnmr@gmail.com>
2024-06-24 23:51:35 +0900

    [Add] CountDownScriptの追加

---
+++ RunGame/Assets/Scripts/CountDown.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountDown : MonoBehaviour
+{
+    [Tooltip("この時間からカウントダウンを始める")]
+    [SerializeField] private float _num;
+
+    [SerializeField] private Text _text = null;
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        _text.text = _num.ToString("0");
+        if (_num <= 0.5f)
+        {
+            _text.text = "Start!!";
+            Invoke("Delete", 2f);
+            return;
+        }
+        _num -= Time.deltaTime;
+    }
+
+    void Delete()
+    {
+        _text.enabled = false;
+    }
+}
