c19bdda4aa8d4b02291613aa33e317b4e3a4dc26
YamamotoU518 <hw37hnmr@gmail.com>
2024-06-24 23:43:09 +0900

    [Fix] GameManagerの修正

--- RunGame/Assets/Scripts/GameManager.cs
+++ RunGame/Assets/Scripts/GameManager.cs
@@ -2,13 +2,16 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance = null;
 
     /// <summary> Gameが始まっているか </summary>
-    public bool _isPlaying { get; set;}
+    public bool _isPlaying { get; private set;}
+
+    [SerializeField] private Text _text = null;
     
     private void Awake()
     {
@@ -35,5 +38,11 @@ public class GameManager : MonoBehaviour
     void GameStart()
     {
         _isPlaying = true;
+    }
+
+    public void Finish()
+    {
+        _isPlaying = false;
+        _text.enabled = true;
     }
 }
