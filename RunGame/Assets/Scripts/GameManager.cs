f59aba902b121e6fd415874b22b143247e308ce7
YamamotoU518 <hw37hnmr@gmail.com>
2024-06-25 16:09:49 +0900

    [Fix] Gameが始まってるかをStateで管理するようにした

--- RunGame/Assets/Scripts/GameManager.cs
+++ RunGame/Assets/Scripts/GameManager.cs
@@ -4,14 +4,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+public enum GameState
+{
+    BeforeGame,
+    IsPlaying,
+    Finish
+}
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance = null;
 
-    /// <summary> Gameが始まっているか </summary>
-    public bool _isPlaying { get; private set;}
-
     [SerializeField] private Text _text = null;
+    [SerializeField] private SceneLoader _sceneLoader = null;
+
+    /// <summary> Gameのステート </summary>
+    public GameState _gameState { get; private set; }
     
     private void Awake()
     {
@@ -21,8 +28,8 @@ public class GameManager : MonoBehaviour
             return;
         }
         Destroy(this.gameObject);
-
-        _isPlaying = false;
+        
+        _gameState = GameState.BeforeGame;
     }
 
     void Start()
@@ -37,12 +44,19 @@ public class GameManager : MonoBehaviour
 
     void GameStart()
     {
-        _isPlaying = true;
+        _gameState = GameState.IsPlaying;
+        SEManager.Instance.PlayBGM();
     }
 
     public void Finish()
     {
-        _isPlaying = false;
+        _gameState = GameState.Finish;
         _text.enabled = true;
+        Invoke("LoadScene", 2f);
+    }
+
+    void LoadScene()
+    {
+        _sceneLoader.SceneLoad("Result");
     }
 }
