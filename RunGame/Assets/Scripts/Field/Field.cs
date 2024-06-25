6b35ad64c2ca785eabb8bc7a37da55284e39682f
YamamotoU518 <hw37hnmr@gmail.com>
2024-06-25 16:17:25 +0900

    [Fix] FieldDataまで全部消すようにした

--- RunGame/Assets/Scripts/Field/Field.cs
+++ RunGame/Assets/Scripts/Field/Field.cs
@@ -7,6 +7,9 @@ public class Field : MonoBehaviour
     void Update()
     {
         // プレイヤーが自分（Field）より前に行ったら（自分が画面外に行ったら）消す
-        if (Player.Instance.transform.position.z > gameObject.transform.position.z + 25) { Destroy(this.gameObject);}
+        if (Player.Instance.transform.position.z > gameObject.transform.position.z + 25)
+        {
+            Destroy(this.transform.parent.gameObject);
+        }
     }
 }
